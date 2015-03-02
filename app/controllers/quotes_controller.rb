3ae89092f7cb7aea56f5b565968c4d768cb5ae03
Chloe Bradley <ccbradley77@gmail.com>
2015-03-01 21:08:51 -0700

    fully crudded and functional

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,47 @@
+class QuotesController <ApplicationController
+  def index
+    @quotes = Quote.all
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to quotes_path
+    else
+      render:new
+    end
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+  end
+
+  def edit
+    @quote = Quote.find(params[:id])
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    if @quote.update(quote_params)
+      redirect_to quotes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    quote = Quote.find(params[:id])
+    quote.destroy
+    redirect_to quotes_path
+  end
+
+
+private
+  def quote_params
+    params.require(:quote).permit(:author, :content)
+  end
+end
